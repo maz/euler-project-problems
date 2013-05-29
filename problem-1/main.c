038e0a1fe134b1ef7f2ab9d5a2a13ab69e12ca0d
wesk <wes.kendrick@gmail.com>
2013-05-29 18:44:29 -0400

    added the actual files this time

--- problem-1/main.c
+++ problem-1/main.c
@@ -8,16 +8,19 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
 #include <stdio.h>
+//#include <iostream>
+
+
 main(){
 int count = 0;
-
-/*for(int i = 1; i < 1000; ++i){
+int i;
+for(i = 1; i < 1000; ++i){
 
    if((i % 3 == 0) || (i % 5 == 0)) 
       count += i;
 
 }
-*/
+
 printf("%d\n",count);
 return 0;
 }
